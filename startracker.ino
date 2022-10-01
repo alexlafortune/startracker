2824b5fb3d051e31255b37f5271456107c4f8fe1
Alex Lafortune <aplafortune@gmail.com>
2022-10-01 08:51:42 -0400

    Initial commit

---
+++ startracker.ino
@@ -0,0 +1,59 @@
+#define PIN_A 4
+#define PIN_B 5
+#define PIN_C 6
+#define PIN_D 7
+#define PIN_SWITCH 2
+#define STEPS_PER_REV 512
+
+bool isEnabled;
+const int numSteps = 8;
+int delays[numSteps];
+int steps[numSteps] = {
+	0b1000,
+	0b1100,
+	0b0100,
+	0b0110,
+	0b0010,
+	0b0011,
+	0b0001,
+	0b1001
+};
+
+void setup()
+{
+	pinMode(PIN_A, OUTPUT);
+	pinMode(PIN_B, OUTPUT);
+	pinMode(PIN_C, OUTPUT);
+	pinMode(PIN_D, OUTPUT);
+	pinMode(PIN_SWITCH, INPUT);
+	
+	const int totalDelay = 48;
+	
+	for (int i = 0; i < numSteps; ++i)
+		delays[i] = totalDelay / numSteps + (i < totalDelay % numSteps ? 1 : 0);
+}
+
+void write(int b)
+{
+	digitalWrite(PIN_A, bitRead(b, 0));
+	digitalWrite(PIN_B, bitRead(b, 1));
+	digitalWrite(PIN_C, bitRead(b, 2));
+	digitalWrite(PIN_D, bitRead(b, 3));
+}
+
+void step()
+{
+	for (int i = 0; i < numSteps; ++i)
+	{
+		write(steps[i]);
+		delay(delays[i]);
+	}
+}
+
+void loop()
+{
+	isEnabled = digitalRead(PIN_SWITCH);
+	
+	if (isEnabled)
+		step();
+}
