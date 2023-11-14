15ed091c1a8dcbc6d749d206ef3518c0c80eeace
Alex Lafortune <aplafortune@gmail.com>
2023-11-13 20:38:46 -0500

    Added 8-step delay distribution for finer adjustment.

--- startracker.ino
+++ startracker.ino
@@ -1,19 +1,28 @@
 #define DIR_PIN 2
 #define STEP_PIN 3
 
-const int stepDelay = 1;
+const int numSteps = 8;
+int delays[numSteps];
+int totalDelay = 168;
 
 void setup()
 {
 	pinMode(DIR_PIN, OUTPUT);
 	pinMode(STEP_PIN, OUTPUT);
-	digitalWrite(DIR_PIN, HIGH);	// HIGH or LOW to change direction
+	digitalWrite(DIR_PIN, LOW);	// HIGH or LOW to change direction
+	
+	
+	for (int i = 0; i < numSteps; ++i)
+		delays[i] = totalDelay / numSteps + (i < totalDelay % numSteps ? 1 : 0);
 }
 
 void loop()
 {
-	digitalWrite(STEP_PIN, HIGH);
-	delay(stepDelay);
-	digitalWrite(STEP_PIN, LOW);
-	delay(stepDelay);
+	for (int i = 0; i < numSteps; ++i)
+	{		
+		digitalWrite(STEP_PIN, HIGH);
+		delay(delays[i]);
+		digitalWrite(STEP_PIN, LOW);
+		delay(delays[i]);
+	}
 }
